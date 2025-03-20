c71de74eac5563403a19cb584ff068940b0f89d7
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-03-20 17:25:31 +0100

    Initial commit

---
+++ src/test/java/andrea_freddi/U5_W1_D4_J/U5W1D4JApplicationTests.java
@@ -0,0 +1,13 @@
+package andrea_freddi.U5_W1_D4_J;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W1D4JApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
